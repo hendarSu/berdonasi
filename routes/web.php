9005de1ae958d9b09d9e08789e9d28a7b63ebe5d
Hendar <hendarsundana@gmail.com>
2025-10-06 15:24:57 +0700

    revet

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,5 @@ Route::get('/berita/{slug}', [NewsController::class, 'show'])->name('news.show')
 // Static Pages (storefront)
 Route::get('/p/{slug}', [PageController::class, 'show'])->name('page.show');
 
+// Admin login (POST handler for HTML forms). Filament serves GET /admin/login.
+Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
