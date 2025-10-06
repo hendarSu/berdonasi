01e18dd85ad443e60b814f0bad84e455218759d4
Hendar <hendarsundana@gmail.com>
2025-10-06 21:18:55 +0700

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>
-        {{ env('APP_NAME') }} — Kampanye
+        {{ env('APP_NAME') }} — Beranda
     </title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @php
